ccde7471acb16c6665a38dbce2c4dad989654ee4
Serafinnn <lukasz.serafin.tsm@gmail.com>
2026-07-20 22:06:18 +0200

    Add initial NixOS flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+    description = "Declarative NixOS homelab infrastructure";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+        disko = {
+            url = "github:nix-community/disko";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = {
+        self,
+        nixpkgs,
+        disko,
+        ...
+    }@inputs:
+    {
+        nixosConfigurations = {
+            k72jr = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+
+                specialArgs = {
+                    inherit inputs;
+                };
+
+                modules = [
+                    disko.nixosModules.disko
+                    ./hosts/k72jr
+                ];
+            };
+        };
+    };
+}
---
+++ hosts/k72jr/default.nix
@@ -0,0 +1,11 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  networking.hostName = "k72jr";
+  system.stateVersion = "26.05";
+}
