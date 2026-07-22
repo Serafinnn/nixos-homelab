78023643306bdc2c98b32f2536a499a55d989dea
Serafinnn <s22940@pjwstk.edu.pl>
2026-07-22 20:46:08 +0200

    Added base colmena setup

--- flake.nix
+++ flake.nix
@@ -8,28 +8,77 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    colmena = {
+      url = "github:nix-community/colmena";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    disko,
-    ...
-  }@inputs:
-  {
-    nixosConfigurations = {
-      k72jr = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+  outputs =
+    inputs@{
+      nixpkgs,
+      disko,
+      colmena,
+      ...
+    }:
+
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      k72jrModules = [
+        disko.nixosModules.disko
+        ./hosts/k72jr
+      ];
+    in
+    {
+      nixosConfigurations.k72jr = nixpkgs.lib.nixosSystem {
+        inherit system;
 
         specialArgs = {
           inherit inputs;
         };
 
-        modules = [
-          disko.nixosModules.disko
-            ./hosts/k72jr
+        modules = k72jrModules;
+      };
+
+      colmenaHive = colmena.lib.makeHive {
+        meta = {
+          nixpkgs = pkgs;
+
+          specialArgs = {
+            inherit inputs;
+          };
+
+          allowApplyAll = false;
+        };
+
+        k72jr = {
+          imports = k72jrModules;
+
+          deployment = {
+            targetHost = "k72jr";
+            targetUser = "serafin";
+
+            tags = [
+              "servers"
+              "homelab"
+              "legacy-hardware"
+            ];
+
+            buildOnTarget = false;
+          };
+        };
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          colmena.packages.${system}.colmena
         ];
       };
     };
-  };
 }
